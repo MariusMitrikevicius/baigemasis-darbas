f9019b9c0542877a26e0da0973aa92965d504f81
MariusMitrikevicius <zandenis@gmail.com>
2025-03-25 20:14:58 +0200

    Initial commit

---
+++ src/main/java/lt/mariaus/baigemasisdarbas/BaigemasisDarbasApplication.java
@@ -0,0 +1,13 @@
+package lt.mariaus.baigemasisdarbas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaigemasisDarbasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaigemasisDarbasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/mariaus/baigemasisdarbas/BaigemasisDarbasApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.mariaus.baigemasisdarbas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaigemasisDarbasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
