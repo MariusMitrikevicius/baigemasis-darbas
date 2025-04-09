5765a5865ed99da02f19c857a1250bb76e4572c8
MariusMitrikevicius <zandenis@gmail.com>
2025-04-09 18:43:39 +0300

    task2

--- src/main/java/lt/mariaus/baigiamasis_darbas/BaigemasisDarbasApplication.java
+++ src/main/java/lt/mariaus/baigiamasis_darbas/BaigemasisDarbasApplication.java
@@ -1,25 +1,14 @@
 package lt.mariaus.baigiamasis_darbas;
 
-import lombok.RequiredArgsConstructor;
-import lt.mariaus.baigiamasis_darbas.service.DetaleService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class BaigemasisDarbasApplication {
 
-	private final DetaleService detaleService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaigemasisDarbasApplication.class, args);
 	}
 
-//	@EventListener(ApplicationReadyEvent.class)
-//	public void insertTestData() {
-//		detaleService.pridetiTestinesDetales();
-//
-//	}
 }
--- src/main/java/lt/mariaus/baigiamasis_darbas/service/DetaleService.java
+++ src/main/java/lt/mariaus/baigiamasis_darbas/service/DetaleService.java
@@ -1,9 +1,7 @@
 package lt.mariaus.baigiamasis_darbas.service;
 
 import lombok.RequiredArgsConstructor;
-import lt.mariaus.baigiamasis_darbas.entity.Automobilis;
 import lt.mariaus.baigiamasis_darbas.entity.Detale;
-import lt.mariaus.baigiamasis_darbas.entity.Sandelys;
 import lt.mariaus.baigiamasis_darbas.repository.DetaleRepository;
 import org.springframework.stereotype.Service;
 
@@ -23,78 +21,5 @@ public class DetaleService {
         return detaleRepository.saveAndFlush(detale);
     }
 
-//    public void pridetiTestinesDetales() {
-//        // 1. Padanga
-//        Detale detale1 = new Detale();
-//        detale1.setPavadinimas("Padanga");
-//        detale1.setKaina("300");
-//        detale1.setKiekis(4L);
-//
-//        Automobilis auto1 = new Automobilis();
-//        auto1.setVinKodas("1C4RJFCG9KC687917");
-//        auto1.setMarke("Jeep");
-//
-//        Sandelys sand1 = new Sandelys();
-//        sand1.setPavadinimas("Padangų sandėlys");
-//        sand1.setAdresas("Klevų g. 1");
-//
-//        detale1.setAutomobilis(auto1);
-//        detale1.setSandelys(sand1);
-//        pridetiDetale(detale1);
-//
-//        // 2. Stabdžių kaladėlės
-//        Detale detale2 = new Detale();
-//        detale2.setPavadinimas("Stabdžių kaladėlės");
-//        detale2.setKaina("150");
-//        detale2.setKiekis(8L);
-//
-//        Automobilis auto2 = new Automobilis();
-//        auto2.setVinKodas("WBAFR1C50BC678342");
-//        auto2.setMarke("BMW");
-//
-//        Sandelys sand2 = new Sandelys();
-//        sand2.setPavadinimas("Stabdžių detalių sandėlys");
-//        sand2.setAdresas("Gedimino pr. 45");
-//
-//        detale2.setAutomobilis(auto2);
-//        detale2.setSandelys(sand2);
-//        pridetiDetale(detale2);
-//
-//        // 3. Oro filtras
-//        Detale detale3 = new Detale();
-//        detale3.setPavadinimas("Oro filtras");
-//        detale3.setKaina("45");
-//        detale3.setKiekis(12L);
-//
-//        Automobilis auto3 = new Automobilis();
-//        auto3.setVinKodas("JM1GJ1W56F1234567");
-//        auto3.setMarke("Mazda");
-//
-//        Sandelys sand3 = new Sandelys();
-//        sand3.setPavadinimas("Variklio detalių sandėlys");
-//        sand3.setAdresas("Savanorių pr. 12");
-//
-//        detale3.setAutomobilis(auto3);
-//        detale3.setSandelys(sand3);
-//        pridetiDetale(detale3);
-//
-//        // 4. Akumuliatorius
-//        Detale detale4 = new Detale();
-//        detale4.setPavadinimas("Akumuliatorius");
-//        detale4.setKaina("200");
-//        detale4.setKiekis(5L);
-//
-//        Automobilis auto4 = new Automobilis();
-//        auto4.setVinKodas("5YJSA1E21HF123456");
-//        auto4.setMarke("Tesla");
-//
-//        Sandelys sand4 = new Sandelys();
-//        sand4.setPavadinimas("Elektros sistemų sandėlys");
-//        sand4.setAdresas("Technologijų g. 7");
-//
-//        detale4.setAutomobilis(auto4);
-//        detale4.setSandelys(sand4);
-//        pridetiDetale(detale4);
-//    }
 }
 
